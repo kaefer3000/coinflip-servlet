d2de6b21fa0e54647db2f8adcba2abc29610c4ac
Tobias Käfer <tobias_kaefer@yahoo.de>
2017-08-15 13:29:29 +0200

    inital commit

---
+++ src/main/java/edu/kit/aifb/tok/coinflipservlet/CoinFlipServlet.java
@@ -0,0 +1,38 @@
+package edu.kit.aifb.tok.coinflipservlet;
+
+import java.util.Arrays;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.semanticweb.yars.nx.Node;
+import org.semanticweb.yars.nx.Resource;
+import org.semanticweb.yars.nx.namespace.RDF;
+
+@Path("")
+public class CoinFlipServlet {
+	
+	private static final String VOCAB_BASE_URI_STRING = "http://github.com/kaefer3000/coinflip-servlet/vocab#";
+	private static final Resource COINFLIP = new Resource(VOCAB_BASE_URI_STRING + "CoinFlip");
+	private static final Resource RESULT = new Resource(VOCAB_BASE_URI_STRING + "result");
+	private static final Resource HEADS = new Resource(VOCAB_BASE_URI_STRING + "Heads");
+	private static final Resource TAILS = new Resource(VOCAB_BASE_URI_STRING + "Tails");
+
+	@GET
+	public Response getTime(@Context UriInfo uriinfo) {
+		
+		Resource nir = new Resource(uriinfo.getAbsolutePath().toString() + "#it");
+
+		Node[][] triples = new Node[][] {
+			new Node[] { nir, RDF.TYPE, COINFLIP },
+			new Node[] { nir, RESULT, Math.random()<0.5?HEADS:TAILS }};
+
+		return Response.ok(new GenericEntity<Iterable<Node[]>>(Arrays.asList(triples)) {
+		}).build();
+
+	}
+}
---
+++ src/main/java/edu/kit/aifb/tok/coinflipservlet/CoinFlipServletContext.java
@@ -0,0 +1,55 @@
+package edu.kit.aifb.tok.coinflipservlet;
+
+import java.util.EnumSet;
+import java.util.logging.Logger;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRegistration;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class CoinFlipServletContext implements ServletContextListener {
+
+	final static Logger _log = Logger.getLogger(CoinFlipServletContext.class.getName());
+
+	ServletContext _ctx;
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+
+		_ctx = sce.getServletContext();
+
+		// Register Servlet
+		ServletRegistration sr = _ctx.addServlet("Publishing the results of the tossing of a coin",
+				org.glassfish.jersey.servlet.ServletContainer.class);
+		sr.addMapping("/*");
+		sr.setInitParameter(org.glassfish.jersey.server.ServerProperties.PROVIDER_PACKAGES,
+				this.getClass().getPackage().getName() + ","
+						+ org.semanticweb.yars.jaxrs.JerseyAutoDiscoverable.class.getPackage().getName());
+
+		FilterRegistration fr;
+
+		// Take CORS implementations from the containers.
+		if (_ctx.getServerInfo().startsWith("jetty")) {
+			fr = _ctx.addFilter("cross-origin", "org.eclipse.jetty.servlets.CrossOriginFilter");
+			fr.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
+		} else if (_ctx.getServerInfo().toLowerCase().contains("tomcat")) {
+			fr = _ctx.addFilter("cross-origin", "org.apache.catalina.filters.CorsFilter");
+			fr.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
+		} else {
+			_log.warning("Please configure CORS for your server.");
+		}
+
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
